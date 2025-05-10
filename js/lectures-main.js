9e25e685e45898ed69d4c030c98a42f59a343109
kareemRafat <kareem.force@gmail.com>
2025-05-10 21:10:34 +0300

    fix badge position

--- js/lectures-main.js
+++ js/lectures-main.js
@@ -80,15 +80,16 @@ function setCard(lec) {
     </div>
     <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
     </div>
-    <div class="-mt-[2px] mt-3 sm:pe-0 pb-7 border-indigo-500 border-b-2">
+    <div class="mt-3 sm:pe-0 pb-7 border-indigo-500 border-b-2">
       <div class="flex items-center gap-2">
-        <i class="fas fa-circle-check mb-2 text-zinc-500"></i>
-        <h3 class="text-xl mb-2 font-semibold text-amber-500 dark:text-white">${capitalizeFirstLetter(
+        <i class="fas fa-circle-check text-zinc-500"></i>
+        <h3 class="text-xl mb-1 font-semibold text-amber-500 dark:text-white">${capitalizeFirstLetter(
           lec.group_name
-        )}<span class="ml-2 inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-yellow-600/20 ring-inset ">${lec.group_time == 2 || lec.group_time == 5 ? lec.group_time + ' - Friday' : lec.group_time}</span></h3>
+        )}</h3>
+        <span class="ml-2 inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-yellow-600/20 ring-inset ">${lec.group_time == 2 || lec.group_time == 5 ? lec.group_time + ' - Friday' : lec.group_time}</span>
       </div>
       
-      <div class="block mb-4 text-md font-normal leading-none text-gray-500 dark:text-gray-500"><i class="fab fa-teamspeak mr-1"></i> Instructor :  ${
+      <div class="block mt-1 mb-4 text-md font-normal leading-none text-gray-500 dark:text-gray-500"><i class="fab fa-teamspeak mr-1"></i> Instructor :  ${
         capitalizeFirstLetter(lec.instructor_name)
       }</div>
 
