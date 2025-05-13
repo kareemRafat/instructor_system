91a2b05b1bd2693f7218f0b3a760c997e695d3bb
= <kareem.force@gmail.com>
2025-05-13 03:02:10 +0300

    add database file

--- createivo.sql
+++ createivo.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 11, 2025 at 05:39 PM
--- Server version: 11.7.2-MariaDB-log
--- PHP Version: 8.2.25
+-- Host: 127.0.0.1
+-- Generation Time: May 13, 2025 at 02:01 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -94,7 +94,8 @@ INSERT INTO `groups` (`id`, `name`, `time`, `instructor_id`, `branch_id`, `is_ac
 (29, 'zz', 0.00, 2, 2, 0, '2025-05-10 14:27:57', '2025-05-10 14:45:24'),
 (30, 'monday', 0.00, 5, 1, 0, '2025-05-10 15:34:37', '2025-05-10 15:48:02'),
 (31, 'Hover', 12.30, 6, 1, 1, '2025-05-10 19:38:57', NULL),
-(32, 'this is test', 2.00, 6, 1, 1, '2025-05-10 19:45:56', NULL);
+(32, 'this is test', 2.00, 6, 1, 1, '2025-05-10 19:45:56', NULL),
+(33, '555555asasdsad', 3.00, 2, 2, 0, '2025-05-13 02:01:50', '2025-05-13 02:35:09');
 
 -- --------------------------------------------------------
 
@@ -116,16 +117,16 @@ CREATE TABLE `instructors` (
 --
 
 INSERT INTO `instructors` (`id`, `username`, `password`, `branch_id`, `is_active`, `role`) VALUES
-(1, 'kareem', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 1, 0, 'admin'),
-(2, 'magdy', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 2, 0, 'inst'),
-(3, 'sobhy', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 3, 0, 'inst'),
-(4, 'assim', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 3, 1, 'inst'),
-(5, 'nora', '123', 1, 0, 'inst'),
-(6, 'esraa', '123', 1, 0, 'inst'),
-(7, 'asdsad', '$2y$10$jUQdd92yLTSN4mmk7TkkaOhIuHxYS3TPx8.aEFM1lyeVJoFvoqa86', NULL, 1, 'instructor'),
+(1, 'kareem', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 1, 1, 'admin'),
+(2, 'magdy', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 2, 1, 'inst'),
+(3, 'sobhy', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 3, 1, 'inst'),
+(4, 'assim', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 3, 1, 'instructor'),
+(5, 'hala', '$2y$10$HtisKuRB1rUWCrToSZ5D6OPBBEO9/deo.Z81s/lov5WYpnK0BLbuK', 1, 1, 'cs'),
+(6, 'esraa', '123', 1, 1, 'cs'),
+(7, 'asdsad', '$2y$10$jUQdd92yLTSN4mmk7TkkaOhIuHxYS3TPx8.aEFM1lyeVJoFvoqa86', NULL, 0, 'instructor'),
 (8, 'mostafa', '$2y$10$mtuJJFP3v7EW.SOoSnzBq.N8.PKhpOaJkaqKGnV8R0fCCRpywWoba', NULL, 1, 'instructor'),
 (9, 'khaled', '$2y$10$13oIR41j2hADa3GZnQO8PuqA6YCCgSKPKP9YInRRhxwfSEc/BWq4m', 2, 1, 'instructor'),
-(10, 'omar', '$2y$10$m2REgLOKjGD8tKxngLF9NOaAcewZjrGA.EVF/vt6RvLln8Tjdnigi', 2, 1, 'instructor');
+(10, 'omar', '$2y$10$m2REgLOKjGD8tKxngLF9NOaAcewZjrGA.EVF/vt6RvLln8Tjdnigi', 2, 0, 'instructor');
 
 -- --------------------------------------------------------
 
@@ -256,7 +257,7 @@ ALTER TABLE `branches`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `instructors`
