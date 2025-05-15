2db2c45bc6c8596bb2071d6ac06b5cc95f78dc16
= <kareem.force@gmail.com>
2025-05-16 02:02:00 +0300

    add dropdown in navbar

--- Design/includes/navbar.php
+++ Design/includes/navbar.php
@@ -21,45 +21,68 @@ $pageName = basename($_SERVER['PHP_SELF']);
                 <li>
                     <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page"></a>
                 </li>
-                <?php
-                if (ROLE === 'admin') {
-                ?>
-                    <li>
-                        <a href="instructors.php" class=" <?= $pageName == 'instructors.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Instructors</a>
-                    </li>
-                <?php } ?>
+                <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
                 <?php
                 if (ROLE === 'admin' or ROLE === 'cs-admin' or ROLE === 'cs') {
                 ?>
-                <li>
-                    <a href="lectures.php" class=" <?= $pageName == 'lectures.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Lectures</a>
-                </li>
-                <?php } ?>
-                <?php
-                if (ROLE == 'admin' or ROLE == 'cs-admin') {
-                ?>
                     <li>
-                        <a href="customer-service.php" class=" <?= $pageName == 'customer-service.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Customer service</a>
+                        <a href="lectures.php" class=" <?= $pageName == 'lectures.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Lectures</a>
                     </li>
                 <?php } ?>
-                 <?php
-                if (ROLE == 'admin' or ROLE == 'cs' or ROLE == 'cs-admin') {
-                ?>
-                    <li>
-                        <a href="groups.php" class=" <?= $pageName == 'groups.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Groups</a>
-                    </li>
-                <?php } ?>
-                <?php
-                if (ROLE === 'admin' or ROLE === 'instructor') {
-                ?>
-                    <li>
-                        <a href="index.php" class=" <?= $pageName == 'index.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add Lecture</a>
-                    </li>
-                <?php } ?>
-                <li>
-                    <a href="functions/Auth/logout.php" class="block py-2 px-3 text-red-700 rounded-sm hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
+                <!-- crew dropDown -->
+                <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3  <?= $pageName == 'instructors.php' || $pageName == 'customer-service.php' ? 'text-blue-600' : 'text-gray-900' ?> rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-green-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Crew <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg></button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400 text-base font-semibold" aria-labelledby="dropdownLargeButton">
+                                    <!-- li -->
+                                    <?php
+                                    if (ROLE === 'admin') {
+                                    ?>
+                                        <li>
+                                            <a href="instructors.php" class=" <?= $pageName == 'instructors.php' ? 'text-blue-600' : '' ?> block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ">Instructors</a>
+                                        </li>
+                                    <?php } ?>
+                                    <?php
+                                    if (ROLE == 'admin' or ROLE == 'cs-admin') {
+                                    ?>
+                                        <li>
+                                            <a href="customer-service.php" class=" <?= $pageName == 'customer-service.php' ? 'text-blue-600' : '' ?> block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ">Customer service</a>
+                                        </li>
+                        </li>
+                    <?php } ?>
+                    </ul>
+                </div>
                 </li>
             </ul>
         </div>
+        <?php
+        if (ROLE == 'admin' or ROLE == 'cs' or ROLE == 'cs-admin') {
+        ?>
+            <li>
+                <a href="groups.php" class=" <?= $pageName == 'groups.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Groups</a>
+            </li>
+        <?php } ?>
+        <?php
+        if (ROLE === 'admin' or ROLE === 'instructor') {
+        ?>
+            <li>
+                <a href="index.php" class=" <?= $pageName == 'index.php' ? 'text-blue-600' : '' ?> block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add Lecture</a>
+            </li>
+        <?php } ?>
+        <li>
+            <a href="functions/Auth/logout.php" class="block py-2 px-3 text-red-700 rounded-sm hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
+        </li>
+        </ul>
+    </div>
     </div>
 </nav>
