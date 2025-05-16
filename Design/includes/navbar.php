e166b18d5b1488ba0358c8938fdc0a86c1d752fd
= <kareem.force@gmail.com>
2025-05-16 15:28:07 +0300

    fix team hidden login when instrucor

--- Design/includes/navbar.php
+++ Design/includes/navbar.php
@@ -30,7 +30,7 @@ $pageName = basename($_SERVER['PHP_SELF']);
                 </li>
                 <!-- crew drop down -->
                 <li>
-                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 <?= $pageName == 'instructors.php' || $pageName == 'customer-service.php' ? 'text-blue-600' : 'text-gray-900' ?> rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Team <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 <?= $pageName == 'instructors.php' || $pageName == 'customer-service.php' ? 'text-blue-600' : 'text-gray-900' ?> rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent <?= ROLE !== 'admin' && ROLE !== 'cs-admin' ? 'hidden' : '' ?>">Team <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
                         </svg></button>
                     <!-- Dropdown menu -->
