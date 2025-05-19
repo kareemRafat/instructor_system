bd6283d9f23f68ac4e8ac1b782ea72f109da5dab
kareemRafat <kareem.force@gmail.com>
2025-05-19 17:35:52 +0300

    delete lectures when finish groups

--- functions/Groups/finish_group.php
+++ functions/Groups/finish_group.php
@@ -13,14 +13,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($group_id) {
         try {
+            // Start transaction
+            $pdo->beginTransaction();
+
+            // Update group to set is_active to 0 and set finish_date
             $stmt = $pdo->prepare("UPDATE `groups` SET is_active = 0 , finish_date = '$finist_date' WHERE id = :group_id");
             $stmt->bindParam(':group_id', $group_id);
             $stmt->execute();
+
+            // delete from lectures when the group is finished
+            $stmtDel = $pdo->prepare("DELETE FROM lectures WHERE group_id = :group_id");
+            $stmtDel->bindParam(':group_id', $group_id);
+            $stmtDel->execute();
+
+            // Commit transaction if both queries succeeded
+            $pdo->commit();
 
             // Return JSON response
             header('Content-Type: application/json');
             echo json_encode(['status' => 'success', 'message' => 'Group finished successfully']);
         } catch (PDOException $e) {
+            // Roll back transaction if any error occurs
+            $pdo->rollBack();
+
             header('Content-Type: application/json');
             echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
         }
--- functions/Lectures/insert_lecture.php
+++ functions/Lectures/insert_lecture.php
@@ -21,15 +21,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $date = $_POST['date'] ?? null;
 
     try {
-        // Start transaction
-        $pdo->beginTransaction();
-
-        // delete the last comment before insert new ONE
-        $delStmt =  $pdo->prepare("DELETE FROM lectures WHERE group_id = :group AND instructor_id = :instructor");
-        $delStmt->bindParam(':group', $group);
-        $delStmt->bindParam(':instructor', $_SESSION['user_id'], PDO::PARAM_INT);
-        $delStmt->execute();
-
         // insert New Comment
         $stmt = $pdo->prepare("INSERT INTO lectures (group_id, track_id, instructor_id, comment , date) VALUES (:group, :track, :instructor,  :comment , :date )");
         $stmt->bindParam(':group', $group);
@@ -39,16 +30,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt->bindParam(':date', $date);
         $stmt->execute();
 
-        // Commit transaction if both queries succeeded
-        $pdo->commit();
-
         $_SESSION['success'] = "Data added successfully";
         header("Location: ../../index.php");
     } catch (PDOException $e) {
-
-        // Roll back transaction if any error occurs
-        $pdo->rollBack();
-
         $_SESSION['errors'] = $e->getMessage();
         echo "Error: " . $e->getMessage();
     }
