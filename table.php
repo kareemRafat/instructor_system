50cf4320586b784c59bc04c00ff983fa1a2c8005
kareemRafat <kareem.force@gmail.com>
2025-05-25 18:14:08 +0300

    add header to table.php

--- table.php
+++ table.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instructor Schedule</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body>
+<?php
+include_once 'Helpers/bootstrap.php';
+include_once 'Design/includes/header.php';
+include_once 'Design/includes/navbar.php';
+?>
     <div class="p-6 bg-gray-50 min-h-screen">
         <div class="max-w-6xl mx-auto">
             <h1 class="text-3xl font-bold text-center mb-8 text-gray-800">
