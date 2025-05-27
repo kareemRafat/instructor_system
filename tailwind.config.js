09d186aff4e46ec40db52715796a8bfd72034b44
= <kareem.force@gmail.com>
2025-05-27 16:01:58 +0300

    downgrade to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,php}"],
-  darkMode: false,
+  content: ["./**/*.{html,js,php}","./js/*.{html,js,php}"],
+  darkMode: 'media',
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          200: "#bfdbfe",
-          300: "#93c5fd",
-          400: "#60a5fa",
-          500: "#3b82f6",
-          600: "#2563eb",
-          700: "#1d4ed8",
-          800: "#1e40af",
-          900: "#1e3a8a",
-          950: "#172554",
-        },
-      },
-    },
-  },
-  plugins: [],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  variants: {
     extend: {},
   },
-  corePlugins: {
-    float: true,
-    clear: true,
-    objectFit: true,
-    objectPosition: true,
-  },
+  plugins: [],
 };
