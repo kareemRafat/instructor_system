5afa7e4e28ac427d3fd24ca6209c8b8b2957b715
kareemRafat <kareem.force@gmail.com>
2025-05-28 16:32:34 +0300

    make small screen login design

--- login.php
+++ login.php
@@ -30,19 +30,20 @@ if (isset($_SESSION['user_id'])) {
   </style>
 </head>
 
-<body class="bg-gray-100 flex flex-col items-center justify-center h-screen gap-4 bg-[url(https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701456476.jpg)] bg-cover">
+<body class="bg-gray-50 md:bg-gray-50 flex flex-col items-center justify-center h-screen gap-4 md:bg-[url(https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701456476.jpg)] bg-cover">
   <div class="w-full max-w-sm rounded-lg p-6">
-    <h1 class="mb-4 text-center text-3xl tracking-wider text-white">Createivo <span class="underline decoration-8 decoration-blue-400">Instructors</span></h1>
+    <h1 class="mb-4 hidden md:block text-center text-3xl tracking-wider text-blue-900 md:text-white">Createivo <span class="underline decoration-8 decoration-blue-400">Instructors</span></h1>
+    <img class="md:hidden" src="images/login2.svg" alt="">
   </div>
-  <div class="w-full max-w-sm bg-white rounded-lg shadow-md p-6">
-
-    <h2 class="text-2xl font-bold text-center text-gray-700 mb-6">Login</h2>
+  <div class="w-full max-w-md bg-gray-50 md:bg-white  rounded-lg md:shadow-md p-6">
+    <h2 class="text-2xl font-bold text-left md:text-center text-gray-700 mb-2">Login</h2>
+    <p class="text-base font-semibold text-left md:text-center text-gray-700 mb-6">Please sign in to Continue</p>
     <form class="max-w-sm mx-auto" action="functions/Auth/login.php" method="POST">
       <div class="mb-5"> <label for="username" class="block mb-2 text-sm font-medium text-gray-900"> username</label>
-        <input autofocus name="username" type="text" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required />
+        <input autofocus name="username" type="text" id="username" class="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-zinc-500 md:focus:border-blue-500 block w-full p-2.5" placeholder="" required />
       </div>
       <div class="mb-5"> <label for="password" class="block mb-2 text-sm font-medium text-gray-900"> password</label>
-        <input name="password" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+        <input name="password" type="password" id="password" class="bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-zinc-500 md:focus:border-blue-500 block w-full p-2.5" required />
       </div>
       <div class="flex items-start mb-5">
         <div class="flex items-center h-5">
@@ -50,7 +51,7 @@ if (isset($_SESSION['user_id'])) {
         </div>
         <label for="remember" class="ms-2 text-sm font-medium text-gray-900">Remember me</label>
       </div>
-      <button type="submit" class="w-full text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center">Submit</button>
+      <button type="submit" class="w-full text-white bg-zinc-700 md:bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center">Submit</button>
     </form>
 
     <div>
