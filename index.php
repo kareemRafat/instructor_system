15b2642d7da537426638810a9c2a4a92a8390462
= <kareem.force@gmail.com>
2025-05-29 16:54:04 +0300

    fix width index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $errors = $_SESSION['errors'] ?? [];
 <div class="max-w-7xl mx-auto md:pt-6 md:px-6">
     <!-- card -->
     <div class="flex flex-col items-center justify-center md:my-8 text">
-        <div class="w-full max-w-full md:max-w-md bg-white rounded-lg md:shadow-md p-6">
+        <div class="w-full max-w-full md:max-w-[30rem] bg-white rounded-lg md:shadow-md p-6">
             <h2 class="text-2xl font-bold text-center text-gray-700 mb-4">
                 <span class="underline underline-offset-5 decoration-8 decoration-blue-400"><?= ucwords(USERNAME) ?>'s</span> Groups</h2>
             <form action="functions/Lectures/insert_lecture.php" method="POST" class="space-y-6">
@@ -32,13 +32,13 @@ $errors = $_SESSION['errors'] ?? [];
                     }
                     ?>
                 </div>
-                <div class="flex flex-col md:flex-row items-start flex-wrap gap-4 mb-6">
-                    <div class="flex items-center gap-2 bg-amber-50 px-4 py-2 rounded-lg border border-amber-200 w-full md:w-48 shadow-sm">
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
+                    <div class="flex items-center gap-2 bg-amber-50 px-4 py-2 rounded-lg border border-amber-200 w-full  shadow-sm">
                         <svg class="w-4 h-4 text-amber-600" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
                         </svg><span id="start-date" class="text-amber-700 font-medium text-sm">Group Start Date</span>
                     </div>
-                    <div class="flex items-center gap-2 bg-teal-50 px-4 py-2 rounded-lg border border-teal-200 w-full md:w-48 shadow-sm">
+                    <div class="flex items-center gap-2 bg-teal-50 px-4 py-2 rounded-lg border border-teal-200 w-full  shadow-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-teal-500">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"></path>
                         </svg>
