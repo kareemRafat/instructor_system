3b6d6f8640967fdd50fc89059e8ab9ad29938e79
= <kareem.force@gmail.com>
2025-06-15 01:28:39 +0300

    fix logo

--- Design/includes/navbar.php
+++ Design/includes/navbar.php
@@ -9,7 +9,7 @@ $pageName = basename($_SERVER['PHP_SELF']);
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 gap-4">
         <a href="index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
             <span class="flex justify-between self-center text-xl font-semibold whitespace-nowrap">
-                <span class="w-8 h-8 mt-0.5 text-teal-700">
+                <span class="w-8 h-8 mt-0.5 text-blue-900">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                         <path fill-rule="evenodd" d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm3.97.97a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 0 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Zm4.28 4.28a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd" />
                     </svg>
