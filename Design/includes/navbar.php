04bad90b5ebe1e7baee54e6165424f5befe4475c
= <kareem.force@gmail.com>
2025-06-15 02:41:31 +0300

    fix roles

--- Design/includes/navbar.php
+++ Design/includes/navbar.php
@@ -41,7 +41,7 @@ function hasRole(...$roles)
                 </li>
                 <!-- team drop down -->
                 <li>
-                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 <?= $pageName == 'instructors.php' || $pageName == 'customer-service.php' ? 'text-blue-600' : 'text-gray-900' ?> rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto <?= ROLE !== 'admin' && ROLE !== 'cs-admin' ? 'hidden' : '' ?>">Team <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 <?= $pageName == 'instructors.php' || $pageName == 'customer-service.php' ? 'text-blue-600' : 'text-gray-900' ?> rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto <?= ROLE !== 'admin' && ROLE !== 'cs-admin' && ROLE !== 'owner' ? 'hidden' : '' ?>">Team <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
                         </svg></button>
                     <!-- Dropdown menu -->
