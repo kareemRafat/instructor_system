bdbb43ecab44e79db79728d4cca0a32389c67053
kareemRafat <kareem.force@gmail.com>
2025-07-01 18:16:02 +0300

    fix back group show

--- Design/Partials/Groups/show.php
+++ Design/Partials/Groups/show.php
@@ -53,7 +53,9 @@ foreach ($lectures as $lecture) {
         <div>
             <h3 class="text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl"><span class="text-blue-600"><?= ucwords($lecture['group_name']) ?></span> Group </h3>
         </div>
-        <?php $groupPage = isset($_GET['sort']) && $_GET['sort'] == 'asc' ? "groups.php?sort=asc" : "groups.php" ?>
+        <?php 
+            $groupPage = isset($_SESSION['params']) ? "groups.php?".$_SESSION['params'] : "groups.php" ;
+        ?>
         <a href="<?= isset($_SESSION['page']) ? 'tables.php' : $groupPage ; ?>" class="inline-flex items-center justify-center self-end p-2 text-base font-medium text-gray-500 rounded-lg bg-gray-100 hover:text-gray-900 hover:bg-gray-200">
             <svg class="w-4 h-4 me-2 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -111,7 +113,11 @@ foreach ($lectures as $lecture) {
         </div>
     <?php endforeach; ?>
 </div>
+<?php 
 
+    unset($_SESSION['params']);
+
+?>
 <?php
 /** get the last instructor for the group */
 function getLastInstructorName($data)
