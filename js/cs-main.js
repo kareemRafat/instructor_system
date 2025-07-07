1eccc3f0834888c9a88f61584ceff8890fad9a36
= <kareem.force@gmail.com>
2025-07-08 00:04:13 +0300

    add send email when finish group

--- js/cs-main.js
+++ js/cs-main.js
@@ -3,9 +3,17 @@ const tbody = document.querySelector("tbody");
 const addInstructor = document.getElementById("addInstructor");
 
 document.addEventListener("DOMContentLoaded", function () {
+
+  /** search group */
+  const debouncedSearch = debounce(function () {
+    searchFunctionality(this.value);
+  }, 500);
+  searchInput.addEventListener("input", debouncedSearch);
+  /** end search event */
+
   /** search functionality */
-  searchInput.addEventListener("input", function () {
-    const searchValue = this.value.trim();
+  function searchFunctionality(value){
+    const searchValue = value.trim();
     fetch(
       `functions/Customer-service/search_cs.php?search=${encodeURIComponent(
         searchValue
@@ -16,7 +24,8 @@ document.addEventListener("DOMContentLoaded", function () {
         setTable(data);
       })
       .catch((error) => console.error("Error:", error));
-  });
+  }
+  /** end search event */
 
   // Handle toggle status button clicks
   tbody.addEventListener("click", function (e) {
@@ -166,9 +175,13 @@ function setTable(res) {
                     </a>
                   </div>
                 </td>
+                
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    ${instructor.email || `<span class="text-gray-400">Not Assigned</span>`}
+                </td>
                 <td class="px-6 py-4">
                 <div class="flex flex-row justify-start items-center">
-                <svg class=" w-5 h-5 mr-1.5  md:inline " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <svg class=" w-5 h-5 mr-1.5 text-indigo-500 md:inline " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z" clip-rule="evenodd" />
                             </svg>
                     ${
@@ -177,9 +190,6 @@ function setTable(res) {
                         : "Not Assigned"
                     }
                     </div>
-                </td>
-                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    ${instructor.email || `<span class="text-gray-400">Not Assigned</span>`}
                 </td>
                 <td class="px-6 py-4">
                     <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset  ${statusColor}">
@@ -261,4 +271,16 @@ function updateInstructorStatusUI(button, isDisabling) {
   // Update the button text, icon, and styles
   button.innerHTML = `${actionIcon} ${actionText}`;
   button.className = `toggle-status-btn cursor-pointer text-sm font-medium border border-gray-300 py-1 px-2 rounded-lg ${actionColor} hover:underline`;
+}
+
+
+/** debounce in search */
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
 }
