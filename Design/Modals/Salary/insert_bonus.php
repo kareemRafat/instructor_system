6fd93bf554028f47ddabac6d56ad73faed4eb3ab
kareemRafat <kareem.force@gmail.com>
2025-07-09 16:42:56 +0300

    finish overtime modals and sql

--- Design/Modals/Salary/insert_bonus.php
+++ Design/Modals/Salary/insert_bonus.php
@@ -6,7 +6,8 @@
         <div class="relative bg-white rounded-lg shadow-sm ">
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
-                <h3 class="text-xl font-semibold text-gray-900 ">
+                <h3 class="text-lg font-semibold text-gray-900 ">
+                    <span class="text-blue-600 font-semibold"><?= ucwords($agent[0]['username']) ?></span>
                     إضافة مكافأة
                 </h3>
                 <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="add-bonus-modal">
