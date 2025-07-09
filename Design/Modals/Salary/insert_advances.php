93fe175d7be75d6b19b07e4779cc1355d6e31896
= <kareem.force@gmail.com>
2025-07-09 04:20:56 +0300

    add advances function and mysql insert

--- Design/Modals/Salary/insert_advances.php
+++ Design/Modals/Salary/insert_advances.php
@@ -56,7 +56,7 @@
                             input.value = mysqlDatetime;
                         });
                     </script>
-                    <button type="submit" class="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">إضافة</button>
+                    <button type="submit" class="w-full text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">إضافة</button>
                 </form>
             </div>
         </div>
